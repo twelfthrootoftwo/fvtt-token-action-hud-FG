fe20ca233855548d6d067bfb7a1811c0e0e15d0c
u7447030 <u7447030@anu.edu.au>
2024-01-21 21:37:43 +1100

    Create HUD roll buttons

--- scripts/roll-handler.js
+++ scripts/roll-handler.js
@@ -1,108 +1,89 @@
-export let RollHandler = null
+export let RollHandler = null;
 
-Hooks.once('tokenActionHudCoreApiReady', async (coreModule) => {
-    /**
-     * Extends Token Action HUD Core's RollHandler class and handles action events triggered when an action is clicked
-     */
-    RollHandler = class RollHandler extends coreModule.api.RollHandler {
-        /**
-         * Handle action click
-         * Called by Token Action HUD Core when an action is left or right-clicked
-         * @override
-         * @param {object} event        The event
-         * @param {string} encodedValue The encoded value
-         */
-        async handleActionClick (event, encodedValue) {
-            const [actionTypeId, actionId] = encodedValue.split('|')
+Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
+	/**
+	 * Extends Token Action HUD Core's RollHandler class and handles action events triggered when an action is clicked
+	 */
+	RollHandler = class RollHandler extends coreModule.api.RollHandler {
+		/**
+		 * Handle action click
+		 * Called by Token Action HUD Core when an action is left or right-clicked
+		 * @override
+		 * @param {object} event        The event
+		 * @param {string} encodedValue The encoded value
+		 */
+		async handleActionClick(event, encodedValue) {
+			const payload = encodedValue.split("|");
 
-            const renderable = ['item']
+			if (payload.length < 2) {
+				super.throwInvalidValueErr();
+			}
 
-            if (renderable.includes(actionTypeId) && this.isRenderItem()) {
-                return this.doRenderItem(this.actor, actionId)
-            }
+			const actionTypeId = payload[0];
+			const actionId = payload[1];
 
-            const knownCharacters = ['character']
+			const renderable = ["attributeRolls"];
 
-            // If single actor is selected
-            if (this.actor) {
-                await this.#handleAction(event, this.actor, this.token, actionTypeId, actionId)
-                return
-            }
+			if (renderable.includes(actionTypeId) && this.isRenderItem()) {
+				return this.doRenderItem(this.actor, actionId);
+			}
 
-            const controlledTokens = canvas.tokens.controlled
-                .filter((token) => knownCharacters.includes(token.actor?.type))
+			const knownCharacters = ["fisher", "fish"];
 
-            // If multiple actors are selected
-            for (const token of controlledTokens) {
-                const actor = token.actor
-                await this.#handleAction(event, actor, token, actionTypeId, actionId)
-            }
-        }
+			// If single actor is selected
+			if (this.actor) {
+				await this.#handleAction(
+					event,
+					this.actor,
+					this.token,
+					actionTypeId,
+					actionId
+				);
+				return;
+			}
 
-        /**
-         * Handle action hover
-         * Called by Token Action HUD Core when an action is hovered on or off
-         * @override
-         * @param {object} event        The event
-         * @param {string} encodedValue The encoded value
-         */
-        async handleActionHover (event, encodedValue) {}
+			//Multiple actors not supported
+		}
 
-        /**
-         * Handle group click
-         * Called by Token Action HUD Core when a group is right-clicked while the HUD is locked
-         * @override
-         * @param {object} event The event
-         * @param {object} group The group
-         */
-        async handleGroupClick (event, group) {}
+		/**
+		 * Handle action hover
+		 * Called by Token Action HUD Core when an action is hovered on or off
+		 * @override
+		 * @param {object} event        The event
+		 * @param {string} encodedValue The encoded value
+		 */
+		async handleActionHover(event, encodedValue) {
+			//TODO: Show flat attributes
+		}
 
-        /**
-         * Handle action
-         * @private
-         * @param {object} event        The event
-         * @param {object} actor        The actor
-         * @param {object} token        The token
-         * @param {string} actionTypeId The action type id
-         * @param {string} actionId     The actionId
-         */
-        async #handleAction (event, actor, token, actionTypeId, actionId) {
-            switch (actionTypeId) {
-            case 'item':
-                this.#handleItemAction(event, actor, actionId)
-                break
-            case 'utility':
-                this.#handleUtilityAction(token, actionId)
-                break
-            }
-        }
+		/**
+		 * Handle action
+		 * @private
+		 * @param {object} event        The event
+		 * @param {object} actor        The actor
+		 * @param {object} token        The token
+		 * @param {string} actionTypeId The action type id
+		 * @param {string} actionId     The actionId
+		 */
+		async #handleAction(event, actor, token, actionTypeId, actionId) {
+			switch (actionTypeId) {
+				case "attributeRolls":
+					this.#handleRollAction(event, actor, actionId);
+					break;
+			}
+		}
 
-        /**
-         * Handle item action
-         * @private
-         * @param {object} event    The event
-         * @param {object} actor    The actor
-         * @param {string} actionId The action id
-         */
-        #handleItemAction (event, actor, actionId) {
-            const item = actor.items.get(actionId)
-            item.toChat(event)
-        }
-
-        /**
-         * Handle utility action
-         * @private
-         * @param {object} token    The token
-         * @param {string} actionId The action id
-         */
-        async #handleUtilityAction (token, actionId) {
-            switch (actionId) {
-            case 'endTurn':
-                if (game.combat?.current?.tokenId === token.id) {
-                    await game.combat?.nextTurn()
-                }
-                break
-            }
-        }
-    }
-})
+		/**
+		 * Handle item action
+		 * @private
+		 * @param {object} event    The event
+		 * @param {object} actor    The actor
+		 * @param {string} actionId The action id
+		 */
+		#handleRollAction(event, actor, actionId) {
+			//TODO: make this an actual function
+			console.log(actionId);
+			//actor.roll()
+		}
+	};
+});
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,21 +1,24 @@
-import { MODULE } from './constants.js'
+import {MODULE} from "./constants.js";
 
 /**
  * Register module settings
  * Called by Token Action HUD Core to register Token Action HUD system module settings
  * @param {function} coreUpdate Token Action HUD Core update function
  */
-export function register (coreUpdate) {
-    game.settings.register(MODULE.ID, 'displayUnequipped', {
-        name: game.i18n.localize('tokenActionHud.template.settings.displayUnequipped.name'),
-        hint: game.i18n.localize('tokenActionHud.template.settings.displayUnequipped.hint'
-        ),
-        scope: 'client',
-        config: true,
-        type: Boolean,
-        default: true,
-        onChange: (value) => {
-            coreUpdate(value)
-        }
-    })
+export function register(coreUpdate) {
+	game.settings.register(MODULE.ID, "displayUnequipped", {
+		name: game.i18n.localize(
+			"tokenActionHud.hlm.settings.displayUnequipped.name"
+		),
+		hint: game.i18n.localize(
+			"tokenActionHud.hlm.settings.displayUnequipped.hint"
+		),
+		scope: "client",
+		config: true,
+		type: Boolean,
+		default: true,
+		onChange: (value) => {
+			coreUpdate(value);
+		},
+	});
 }
